ba3ef9549094bc3fc1a6439fdb53a54fe62d365c
bhavanag247 <163707464+bhavanag247@users.noreply.github.com>
2026-05-24 16:18:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CamPrint-main/', // your repo name
+  base: '/CamPrint/', // your repo name
 })
